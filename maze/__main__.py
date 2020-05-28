df6a0a6ac2279d05b158311414ff6e732da8d97d
Maryline <marybot@free.fr>
2020-05-28 21:30:41 +0200

    First test on controller

--- maze/__main__.py
+++ maze/__main__.py
@@ -1,5 +1,8 @@
 """Main file."""
 
 from maze.model.mymaze import MyMaze
+from maze.controller import console
 
 MAZE = MyMaze()
+CONTROLLER = console.Moves()
+CONTROLLER.hero_move()
--- maze/controller/console.py
+++ maze/controller/console.py
@@ -1,11 +1,20 @@
-"""Test saisie clavier."""
 
-while True:  # making a loop
-    try:  # if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed("q"):  # if key 'q' is pressed
-            print("You Pressed a key !")
-            break  # finishing the loop
-        else:
-            pass
-    except:
-        break  # if user pressed a key other than the given key the loop will break
+class Moves:
+    """Control the Hero's movements.
+    move up : "Z"
+    move down : "S"
+    move left : "Q"
+    move right : "D"
+    """
+
+    def hero_move(self):
+        """Moves of the hero."""
+        hero_move = input()
+        if hero_move == "Z":
+            print("Hero goes up.")
+        elif hero_move == "S":
+            print("Hero goes down.")
+        elif hero_move == "Q":
+            print("Hero goes left.")
+        elif hero_move == "D":
+            print("Hero goes right.")
