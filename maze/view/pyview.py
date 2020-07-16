adf4a68b992a0eec7698919e8bd1cf1ded71d32e
Maryline <marybot@free.fr>
2020-07-16 22:13:54 +0200

    Update pyview

--- maze/view/pyview.py
+++ maze/view/pyview.py
@@ -1,14 +1,25 @@
 """Pygame view."""
 
 import sys
-import pygame
 from os import sys
+import pygame
+
 
 class PyView:
     """View with pygame."""
 
     def __init__(self):
         """Initialized."""
+
+        pygame.init()
+
+        self.size = width, height = (480, 480)
+        self.background_color = (0, 50, 0)
+        self.screen = pygame.display.set_mode(self.size)
+
+        self.wall = pygame.image.load(
+            "c:/Users/Utilisateur/OneDrive/Documents/prog/projet3/maze/data/wall.png"
+        )
 
     def display(self, maze):
         """Main method."""
@@ -17,24 +28,17 @@ class PyView:
     def display_maze(self, maze):
         """Display the maze."""
 
-        pygame.init()
-
-        size = width, height = (480, 480)
-        background_color = (0, 50, 0)
-
-        screen = pygame.display.set_mode(size)
-        screen.fill(background_color)
-
-        wall = pygame.image.load(
-            "c:/Users/Utilisateur/OneDrive/Documents/prog/projet3/maze/data/wall.png"
-        )
+        self.screen.fill(self.background_color)
 
         for index_y in range(maze.height + 1):
             for index_x in range(maze.width + 1):
                 position = (index_x, index_y)
                 if position in maze.walls:
-                    screen.blit(wall, (position[0]*32, position[1]*32)
-                    pygame.display.flip()
+                    self.screen.blit(
+                        self.wall, (position[0] * 32, position[1] * 32)
+                    )
+
+        pygame.display.flip()
 
         while 1:
             for event in pygame.event.get():
