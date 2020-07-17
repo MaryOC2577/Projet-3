cb507e6a1ff06c4c33b4f053557939836145dbb5
Maryline <marybot@free.fr>
2020-07-17 16:54:39 +0200

    General update

--- maze/config.py
+++ maze/config.py
@@ -7,3 +7,7 @@ MAZE_DIR = BASE_DIR / "maze"
 DATA_DIR = MAZE_DIR / "data"
 MODEL_DIR = MAZE_DIR / "model"
 LEVEL1 = DATA_DIR / "maze1.txt"
+
+IMG_DIR = DATA_DIR / "img"
+
+SPRITE_SIZE = 32
--- maze/view/pyview.py
+++ maze/view/pyview.py
@@ -2,7 +2,10 @@
 
 import sys
 from os import sys
+
 import pygame
+
+from maze import config
 
 
 class PyView:
@@ -17,15 +20,9 @@ class PyView:
         self.background_color = (0, 50, 0)
         self.screen = pygame.display.set_mode(self.size)
 
-        self.wall = pygame.image.load(
-            "c:/Users/Utilisateur/OneDrive/Documents/prog/projet3/maze/data/wall.png"
-        )
-        self.hero = pygame.image.load(
-            "c:/Users/Utilisateur/OneDrive/Documents/prog/projet3/maze/data/hero.png"
-        )
-        self.guardian = pygame.image.load(
-            "c:/Users/Utilisateur/OneDrive/Documents/prog/projet3/maze/data/guardian.png"
-        )
+        self.wall = pygame.image.load(str(config.IMG_DIR / "wall.png"))
+        self.hero = pygame.image.load(str(config.IMG_DIR / "hero.png"))
+        self.guardian = pygame.image.load(str(config.IMG_DIR / "guardian.png"))
 
     def display(self, maze):
         """Main method."""
@@ -39,21 +36,19 @@ class PyView:
         for index_y in range(maze.height + 1):
             for index_x in range(maze.width + 1):
                 position = (index_x, index_y)
+                pygame_position = (
+                    index_x * config.SPRITE_SIZE,
+                    index_y * config.SPRITE_SIZE,
+                )
                 if position in maze.walls:
-                    self.screen.blit(
-                        self.wall, (position[0] * 32, position[1] * 32)
-                    )
+                    self.screen.blit(self.wall, pygame_position)
                 elif position in maze.paths:
                     items_positions = [item.position for item in maze.items]
                     items_char = [item.char for item in maze.items]
                     if position == maze.hero.position:
-                        self.screen.blit(
-                            self.hero, (position[0] * 32, position[1] * 32)
-                        )
+                        self.screen.blit(self.hero, pygame_position)
                     elif position == maze.guardian:
-                        self.screen.blit(
-                            self.guardian, (position[0] * 32, position[1] * 32)
-                        )
+                        self.screen.blit(self.guardian, pygame_position)
 
         pygame.display.flip()
 
