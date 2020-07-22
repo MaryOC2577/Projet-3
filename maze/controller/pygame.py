f0b2064105d6b902dd9c8ae625030898b7a02666
Maryline <marybot@free.fr>
2020-07-22 20:58:36 +0200

    General update

--- maze/controller/pygame.py
+++ maze/controller/pygame.py
@@ -4,14 +4,13 @@ import sys
 
 import pygame
 
-from maze.model.message import messages
-
 
 class InputKeys:
     """Check input keys."""
 
     def __init__(self):
         """Init keys."""
+
         pygame.init()
 
         self.pressed_key = ""
@@ -21,14 +20,14 @@ class InputKeys:
 
         events = pygame.event.get()
         for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     self.pressed_key = "left"
-                if event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT:
                     self.pressed_key = "right"
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     self.pressed_key = "down"
-                if event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP:
                     self.pressed_key = "up"
